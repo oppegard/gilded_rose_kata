8b88a9aee5905261558905fc95215d46bf02de61
Glenn Oppegard <goppegard@pivotal.io>
2016-03-25 18:13:35 -0600

    #update_item_quality

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,54 +1,58 @@
 def update_quality(items)
   items.each do |item|
-    if item.name == 'Conjured Mana Cake'
-      if item.quality != 0
-        if item.sell_in <= 0
-          item.quality -= 4
-        else
-          item.quality -= 2
+    update_item_quality(item)
+  end
+end
+
+def update_item_quality(item)
+  if item.name == 'Conjured Mana Cake'
+    if item.quality != 0
+      if item.sell_in <= 0
+        item.quality -= 4
+      else
+        item.quality -= 2
+      end
+    end
+  elsif item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
+    if item.quality > 0
+      if item.name != 'Sulfuras, Hand of Ragnaros'
+        item.quality -= 1
+      end
+    end
+  else
+    if item.quality < 50
+      item.quality += 1
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        if item.sell_in < 11
+          if item.quality < 50
+            item.quality += 1
+          end
+        end
+        if item.sell_in < 6
+          if item.quality < 50
+            item.quality += 1
+          end
         end
       end
-    elsif item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
+    end
+  end
+  if item.name != 'Sulfuras, Hand of Ragnaros'
+    item.sell_in -= 1
+  end
+  if item.sell_in < 0 && item.name != 'Conjured Mana Cake'
+    if item.name != "Aged Brie"
+      if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+        if item.quality > 0
+          if item.name != 'Sulfuras, Hand of Ragnaros'
+            item.quality -= 1
+          end
         end
+      else
+        item.quality = item.quality - item.quality
       end
     else
       if item.quality < 50
         item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0 && item.name != 'Conjured Mana Cake'
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
       end
     end
   end
