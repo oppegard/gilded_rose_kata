294bee2e14b532bee6cb45f36b3d2ea6feea4528
Glenn Oppegard <goppegard@pivotal.io>
2016-03-25 21:02:52 -0600

    Move Backstage Passes in to case statement.

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -14,44 +14,29 @@ def update_item_quality(item)
   when 'Aged Brie'
     item.sell_in < 0 ? item.quality += 2 : item.quality += 1
     item.quality = [item.quality, 50].min
+  when 'Backstage passes to a TAFKAL80ETC concert'
+    if item.sell_in < 0
+      item.quality = item.quality - item.quality
+    else
+      item.quality += 1
+      item.quality += 1 if item.sell_in < 10
+      item.quality += 1 if item.sell_in < 5
+      item.quality = [item.quality, 50].min
+    end
   else
-    if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+    if item.quality > 0
+      if item.name != 'Sulfuras, Hand of Ragnaros'
+        item.quality -= 1
+      end
+    end
+    if item.sell_in < 0
       if item.quality > 0
         if item.name != 'Sulfuras, Hand of Ragnaros'
           item.quality -= 1
         end
       end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 10
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 5
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-
-    if item.sell_in < 0
-      if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality -= 1
-          end
-        end
-      else
-        item.quality = item.quality - item.quality
-      end
     end
   end
-
 end
 
 # DO NOT CHANGE THINGS BELOW -----------------------------------------
